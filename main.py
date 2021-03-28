f613eb5a67274f8ef65266a9132d34df948f145c
Noaemien <noaemien@gmail.com>
2021-03-28 15:43:51 +0200

    added creation of the q-table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import gym
-
+import numpy as np
+import random as rnd
 
 env = gym.make("Taxi-v3").env
 env.reset()
@@ -7,6 +8,12 @@ env.render()
 
 print("Action Space {}".format(env.action_space))
 print("State Space {}".format(env.observation_space))
+
+action_space_size = env.action_space
+obs_space_size = env.observation_space
+
+q_table = np.zeros((obs_space_size, action_space_size))
+
 
 
 
