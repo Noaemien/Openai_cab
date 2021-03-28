a8f266e90dbc86b4624932898d0c29b4e5b21bf3
Noaemien <noaemien@gmail.com>
2021-03-28 17:14:46 +0200

    training and testing

--- main.py
+++ main.py
@@ -43,10 +43,40 @@ for i in range(1, 100001):
 
         epochs += 1
 
-    if i % 100 == 0:
+    if i % 10000 == 0:
         print(f"Episode: {i}")
 
-        
+
+
+
+
+
+
+
+total_epochs, total_penalties = 0, 0
+episodes = 100
+
+for _ in range(episodes):
+    state = env.reset()
+    epochs, penalties, reward = 0, 0, 0
+    
+    done = False
+    
+    while not done:
+        action = np.argmax(q_table[state])
+        state, reward, done, info = env.step(action)
+
+        if reward == -10:
+            penalties += 1
+
+        epochs += 1
+
+    total_penalties += penalties
+    total_epochs += epochs
+
+print(f"Results after {episodes} episodes:")
+print(f"Average timesteps per episode: {total_epochs / episodes}")
+print(f"Average penalties per episode: {total_penalties / episodes}") 
         
 
 
